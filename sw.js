e8d07377a71a668c14d9164cf62e2c3dbfc3ba37
Gastenhuis1 <ouwerkerk.esmeralda@gmail.com>
2026-08-24 14:14:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='gh-keuken-online-v48';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./gastenhuis-logo.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='gh-keuken-online-v50';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./gastenhuis-logo.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
